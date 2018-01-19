88f5572e6befbfb46fc9ae7b13b59a06eeccc087
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-01-19 14:16:51 +0000

    Create sum arithmetic till n terms.c

---
+++ sum arithmetic till n terms.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int m,a,d,l,sum=0;
+printf("Enter the value of number of values");
+scanf("%d",&m);
+printf("Enter the value of number of series");
+scanf("%d",&a);
+printf("Enter the value of different");
+scanf("%d",&d);
+l=(m*d)+a-1;
+sum=((a+1)+m)/;
+printf("sum is %d",sum);
+return 0;
+}
