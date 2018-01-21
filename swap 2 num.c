e0b3157ba129909bf3c7ab5f0fada31c6f85cc72
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-01-21 11:28:52 +0530

    Create swap 2 num.c

---
+++ swap 2 num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+int i,j,temp;
+printf("Enter the swap numbers:");
+scanf("%d%d",&i,&j);
+temp=i;	
+i=j;
+j=temp;
+printf("The numbers are swaped :%d %d",i,j);
+return 0;
+}
