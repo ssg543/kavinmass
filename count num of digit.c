2c713fa78d19d72610112d7539642e841537e353
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-01-23 16:38:12 +0530

    Create count num of digit.c

---
+++ count num of digit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int x,y=0,z,n;
+printf("Enter the number:");
+scanf("%d",&x);
+z=x;
+while(z>0)
+{
+n=z%10;
+y++;
+z=z/10;
+}
+printf("The number of digit:%d",y);
+}
