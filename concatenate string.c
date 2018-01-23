925bf6de7fe2f76ec4ac8bd446cc032b46fbf7e4
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-01-23 16:46:34 +0530

    Create concatenate string.c

---
+++ concatenate string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    char m[100],n[100];
+    printf("Enter the element:");
+    scanf("%s",&m);
+    scanf("%s",&n);
+    printf("The concatenate string is:%s%s",m,n);
+    
+    
+}
