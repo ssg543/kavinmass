e41477a75c345e39abc93bdd5181a80272145a43
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-01-25 16:44:50 +0530

    Create power 2.c

---
+++ power 2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter the number");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("It is a power of 2");
+    }
+    else
+    {
+        printf("It is not a power of 2");
+    }
+}
