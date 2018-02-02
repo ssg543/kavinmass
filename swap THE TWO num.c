497bfd800db1b98adbafc8f421261e63e5d1d397
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-02-02 15:07:43 +0530

    Update and rename swap two num.c to swap THE TWO num.c

--- swap THE TWO num.c
+++ swap THE TWO num.c
@@ -4,9 +4,11 @@ int main(void)
 int i,j,temp;
 printf("Enter the swap numbers:");
 scanf("%d%d",&i,&j);
+  {
 temp=i;	
 i=j;
 j=temp;
+  }
 printf("The numbers are swaped :%d %d",i,j);
 return 0;
 }
