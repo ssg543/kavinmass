3d80ce65b9818b638a898e53247f04cdf412cd82
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-02-06 10:31:07 +0000

    Create mul of 7.c

---
+++ mul of 7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    printf("Enter the elements:");
+    scanf("%d",&i);
+    if(i%7==0)
+    {
+        printf("The elements Multiple of 7");
+    }
+    else
+    {
+        printf("The elements is not multiple of 7");
+    }
+}
