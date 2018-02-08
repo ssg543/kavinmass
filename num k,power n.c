5f7c74ef08d62c2d54b8047b85f64cec4bded31b
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-02-08 10:56:20 +0000

    Create num k,power n.c

---
+++ num k,power n.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int base,ex,result=1;
+printf("Enter the number:");
+scanf("%d",&base);
+scanf("%d",&ex);
+while(ex!=0)
+{
+result=result*base;
+--ex;
+}
+printf("%d",result);
+}
