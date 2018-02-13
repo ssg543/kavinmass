b8376dbb41818dfdc724a367b1dc39b75ab17174
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-02-13 16:42:58 +0530

    Create find area.c

---
+++ find area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    float a,b,c;
+    printf("Enter the length:");
+    scanf("%f",&a);
+    printf("Enter the breath:");
+    scanf("%f",&b);
+    c=a*b;
+    printf("The area is:%f",c);
+    
+}
