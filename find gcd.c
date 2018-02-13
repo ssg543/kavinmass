48dd0efa9540a2ef538c724ed852b5b35bc9b0f3
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-02-13 16:53:56 +0530

    Update find gcd.c

--- find gcd.c
+++ find gcd.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    int n1, n2, i, gcd;
+    int n1, n2, j, gcd;
 
     printf("Enter two integers: ");
     scanf("%d %d", &n1, &n2);
 
-    for(i=1; i <= n1 && i <= n2; ++i)
+    for(j=1; j <= n1 && j <= n2; ++j)
     {
         
-        if(n1%i==0 && n2%i==0)
-            gcd = i;
+        if(n1%j==0 && n2%j==0)
+            gcd = j;
     }
 
     printf("G.C.D of %d and %d is %d", n1, n2, gcd);
